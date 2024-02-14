6b6680f54c66f58de10b79963d9b311e1fcd31cf
Yamil Quispe <yamiljquispe@gmail.com>
2024-02-14 17:35:15 -0500

    Initial Commit

---
+++ IssaGameVersion1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IssaGameVersion1
+//
+//  Created by Yamil on 2/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IssaGameVersion1/IssaGameVersion1App.swift
@@ -0,0 +1,17 @@
+//
+//  IssaGameVersion1App.swift
+//  IssaGameVersion1
+//
+//  Created by Yamil on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct IssaGameVersion1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
